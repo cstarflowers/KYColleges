514d670336da9c10433e3809245c684622cac7fd
Star ⭐ <91134654+cstarflowers@users.noreply.github.com>
2025-03-20 10:09:29 -0400

    Add temporary banner

--- app/test.tsx
+++ app/test.tsx
@@ -13,8 +13,11 @@ export default function Test() {
         alignItems: "center",
       }}
     >
-    <div className='raised_text'><Text>You are now viewing <b>Test.tsx</b>!</Text></div>
     <Header />
+    <div className="banner"><svg viewBox="0 20 736 240" xmlns="https://www.w3.org/2000/svg"><g>
+     <image xlinkHref="https://i.pinimg.com/736x/5a/13/d6/5a13d69a9efc0c6b08a3444498ebfa8a.jpg"></image>
+    </g></svg></div>
+    <div className='raised_text'><Text>You are now viewing <b>Test.tsx</b>!</Text></div>
     <Footer />
     </View>
   );
